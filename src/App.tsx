37467b86391d40989e3e43b9d041ddefd27b023e
Juliana Madeira <juliwood1980@gmail.com>
2022-07-14 14:31:38 -0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>testando react com typescript</h1>
+      <p>configuraçoes json feitas</p>
+    </div>
+  );
+}
+
+export default App;
